58bdbf851c020331d282d787a8e6366ae1fd7b4e
Igor Kolomiyets <ikolomiyets@gmail.com>
2022-03-03 22:44:36 +0000

    Initialized state machine from database

--- src/main/java/io/iktech/edu/msscssm/services/PaymentServiceImpl.java
+++ src/main/java/io/iktech/edu/msscssm/services/PaymentServiceImpl.java
@@ -7,6 +7,7 @@ import io.iktech.edu.msscssm.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
 import static io.iktech.edu.msscssm.domain.PaymentState.NEW;
@@ -25,16 +26,34 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> authorise(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
         return null;
+    }
+
+    private StateMachine<PaymentState, PaymentEvent> build(Long paymentId) {
+        Payment payment = paymentRepository.getById(paymentId);
+
+        StateMachine<PaymentState, PaymentEvent> sm = stateMachineFactory.getStateMachine(Long.toString(payment.getId()));
+        sm.stop();
+        sm.getStateMachineAccessor().doWithAllRegions(sma -> {
+            sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getState(), null, null, null));
+        });
+        sm.start();
+        return sm;
     }
 }
