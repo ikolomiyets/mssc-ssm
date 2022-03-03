2f9db0efb5f200bce6d8786aa8c583878ef75160
Igor Kolomiyets <ikolomiyets@gmail.com>
2022-03-03 21:33:13 +0000

    Initial commit

---
+++ src/test/java/io/iktech/edu/msscssm/MsscSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package io.iktech.edu.msscssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscSsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
